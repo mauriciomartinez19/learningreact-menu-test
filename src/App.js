699f316ce7e5a01470167cb360f949f113588ac6
Mauricio <moromanya@hotmail.com>
2021-12-23 10:17:20 -0300

    Add title and nav-bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <h1 className='title'>Our Menu</h1>
+      <div className='line' />
+      <div className='select-bar'>
+        <button className='select-item'>All</button>
+        <button className='select-item'>Breakfast</button>
+        <button className='select-item'>Lunch</button>
+        <button className='select-item'>Shakes</button>
+      </div>
+    </div >
   );
 }
+
+
+
 
 export default App;
